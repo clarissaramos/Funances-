f3cc9a5329b180a0f34ecfbab8fb1db5df1b3c0e
Sabrina Zhang <szcamino@gmail.com>
2022-08-18 15:52:16 -0700

    added tab bar for navigation, new pages set up, recolcration

--- Funances!/QuizViewController.swift
+++ Funances!/QuizViewController.swift
@@ -14,16 +14,19 @@ class QuizViewController: UIViewController {
     
     
     @IBAction func Choice1(_ sender: Any) {
+        Answer.text = ("Inorrect! \n A Roth IRA is an individual retirement account that offers tax-free growth and tax-free withdrawals in retirement. Roth IRA rules dictate that as long as you've owned your account for 5 years and you're age 59½ or older, you can withdraw your money when you want to and you won't owe any federal taxes.")
     }
     
     @IBAction func Choice2(_ sender: Any) {
+        Answer.text = ("Inorrect! \n A Roth IRA is an individual retirement account that offers tax-free growth and tax-free withdrawals in retirement. Roth IRA rules dictate that as long as you've owned your account for 5 years and you're age 59½ or older, you can withdraw your money when you want to and you won't owe any federal taxes.")
     }
     @IBAction func Choice3(_ sender: Any) {
-        Answer.text = ("Correct!")
+        Answer.text = ("Correct! \n A Roth IRA is an individual retirement account that offers tax-free growth and tax-free withdrawals in retirement. Roth IRA rules dictate that as long as you've owned your account for 5 years and you're age 59½ or older, you can withdraw your money when you want to and you won't owe any federal taxes.")
         
         
     }
     @IBAction func Choice4(_ sender: Any) {
+        Answer.text = ("Inorrect! \n A Roth IRA is an individual retirement account that offers tax-free growth and tax-free withdrawals in retirement. Roth IRA rules dictate that as long as you've owned your account for 5 years and you're age 59½ or older, you can withdraw your money when you want to and you won't owe any federal taxes.")
     }
 
     
---
+++ GuideViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GuideViewController.swift
+//  Funances!
+//
+//  Created by Sabrina on 8/18/22.
+//
+
+import UIKit
+
+class GuideViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
